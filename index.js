ebbac1fc12d87d7bc6a92f6bbadb4ab78ccc3d9d
asomeJay <museum114@naver.com>
2019-08-26 04:30:50 +0000

    node http server 작성

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function main(){
-	console.log("Hello goorm!");
-}
+const http = require('http');
 
-main();
+const port = 3000;
+
+http.createServer(function(request, response) {
+    console.log('요청 수신.');
+    
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Hello Git!');
+    response.end();
+    
+}).listen(port);
+
+console.log(`서버 시작 - 포트:${port}`);
