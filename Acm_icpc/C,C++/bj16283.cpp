4c6f05488a6a7ff4b03de3f5f0d578e64940da2e
asomeJay <museum114@naver.com>
2019-09-07 21:38:24 +0900

    2018서울지역예선 Farm

--- Acm_icpc/C,C++/bj16283.cpp
+++ Acm_icpc/C,C++/bj16283.cpp
@@ -6,36 +6,16 @@ void same(int a, int b, int n, int w);
 
 int main(void) {
 	int a, b, n, w;
-	int count = 0;
-	int sheep = 0, goat = 0;
-	cin >> a >> b >> n >> w;
-
-	for (int i = 1, j = n - 1; i <= n; i++, j--) {
-		if (i * a + j * b == w) {
-			count++;
-			sheep = i;
-			goat = j;
-		}
-	}
-
-	if (count == 1) {
-		cout << sheep << " " << goat;
-		return 0;
-	}
-	
-	cout << -1;
-	return 0;
-}
-
-/*int main(void) {
-	int a, b, n, w;
 	double S = 0, G = 0;
 	cin >> a >> b >> n >> w;
 
 	same(a, b, n, w);	 //a == b인 케이스 체크
 
-	S = n + ((a * n - w) / ((double)b - a));
-	G = (w - a * n) / ((double)b - a);
+	double numerator = w - a * n;
+	double denominator = b - a;
+
+	S = n - (numerator / denominator);
+	G = (numerator/ denominator);
 
 	if ((S <= 0) || (G <= 0) ||
 		(G - (int)G != 0) || (S - (int)S != 0)) {
@@ -48,12 +28,12 @@ int main(void) {
 }
 
 void same(int a, int b, int n, int w) {
-	if (b - a == 0) {
-		if (a + b == w) {
+	if ((b - a) == 0) {
+		if ((a + b) == w && n == 2) {
 			cout << 1 << " " << 1;
 			exit(0);
 		}
 		cout << -1;
 		exit(0);
 	}
-}*/
+}
