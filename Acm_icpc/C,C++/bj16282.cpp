cd1891d361c2045143d9edab62076285f8ba0cba
asomeJay <museum114@naver.com>
2019-09-07 21:27:59 +0900

    서울지역예선

---
+++ Acm_icpc/C,C++/bj16282.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+	long long int input, lowerLimit, upperLimit;
+	int n = 1;
+	
+	cin >> input;
+	while (1) {
+		lowerLimit = n * pow(2, n) - 1;
+		upperLimit = (n + 1) * pow(2, (n+1)) - 1;
+		if (input > lowerLimit && input <= upperLimit) {
+			break;
+		} 
+		n++;
+	}
+
+	cout << n;
+
+	return 0;
+}
