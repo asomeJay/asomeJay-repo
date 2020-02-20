5b2d94be535edca0159f0625c747bd7cefd85124
asomeJay <museum114@naver.com>
2020-02-21 00:51:10 +0900

    뇌씨발 인터프리터
    하... garbage collector 활용과 초기화를 잘 활용하는 문제였다

--- Acm_icpc/C,C++/boj3954.cpp
+++ Acm_icpc/C,C++/boj3954.cpp
@@ -2,45 +2,45 @@
 
 #include <iostream>
 #include <stack>
-#include <string>
 #include <algorithm>
+#include <vector>
 
-#define MAX_SM 100000 + 1
-#define MAX_SCI 4096
+#define MAX_SCI 10000
 #define LEFT 3
 #define RIGHT 1
-#define MAX_COUNT 500000000
+#define MAX_COUNT 50000000
 #define MINUS 0
 #define PLUS 1
 
 using namespace std;
 
 int sm, sc, si, pointer;
-int num_array[MAX_SM];
-int code_array[MAX_SCI];
+int loop_position[MAX_SCI];
 
 void move_pointer(int dir);
-void calculate(int mode);
+void calculate(vector<int> &num_array, int mode);
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int i, nt_case, t_p, code_size;
-
+	int i, nt_case;
 	cin >> nt_case; // 전체 케이스가 몇 번인가 체크함. 
 
 	for (int t = 1; t <= nt_case; t++) {
-		int inst_pos, program_counter, max_ind, char_pointer;
-		pointer = inst_pos = program_counter = max_ind = char_pointer = 0;
-		string code, in;
+		int inst_pos, program_counter, max_ind, char_pointer, t_p;
+		inst_pos = program_counter = max_ind = char_pointer = 0;
+
 		stack<int> s_pointer;
 
 		cin >> sm >> sc >> si;
-		cin >> code;
-		cin >> in;
+
+		vector<char> code(sc);
+		vector<char>input_char(si);
+		vector<int>num_array(sm, 0);
 
 		for (i = 0; i < sc; i++) {
+			cin >> code[i];
 			if (code[i] == '[') {
 				s_pointer.push(i);
 			}
@@ -48,19 +48,19 @@ int main(void) {
 			else if (code[i] == ']') {
 				t_p = s_pointer.top();
 				s_pointer.pop();
-				code_array[t_p] = i;
-				code_array[i] = t_p;
+				loop_position[t_p] = i;
+				loop_position[i] = t_p;
 			}
 		}
 
-		while ((program_counter <= MAX_COUNT) && (inst_pos < sc)) {
-			if (max_ind < inst_pos)
-				max_ind = inst_pos;
+		for (i = 0; i < si; i++) cin >> input_char[i];
 
+		while ((program_counter <= MAX_COUNT) && (inst_pos != sc)) {
+			max_ind = max(max_ind, inst_pos);
 			switch (code[inst_pos]) {
-			case '-': calculate(MINUS);
+			case '-': calculate(num_array, MINUS);
 				break;
-			case '+': calculate(PLUS);
+			case '+': calculate(num_array, PLUS);
 				break;
 			case '<': move_pointer(LEFT);
 				break;
@@ -68,37 +68,31 @@ int main(void) {
 				break;
 			case '[':
 				if (num_array[pointer] == 0) {
-					inst_pos = code_array[inst_pos];
-					break;
+					inst_pos = loop_position[inst_pos];
 				}
 				break;
 			case ']':
 				if (num_array[pointer] != 0) {
-					inst_pos = code_array[inst_pos];
-					break;
+					inst_pos = loop_position[inst_pos];
 				}
 				break;
-			case '.':
-				break;
 			case ',':
-				if (char_pointer >= in.size())
+				if (char_pointer == si)
 					num_array[pointer] = 255;
 				else
-					num_array[pointer] = in[char_pointer++];
+					num_array[pointer] = input_char[char_pointer++];
 				break;
-			default:
-				cout << "CASE ERROR " << endl;
 			}
 
 			program_counter++;
 			inst_pos++;
 		}
+
 		if (inst_pos == sc)
 			cout << "Terminates\n";
 		else 
-			cout << "Loops " << code_array[max_ind] << " " << max_ind << '\n';
+			cout << "Loops " << loop_position[max_ind] << " " << max_ind << '\n';
 	}
-	
 	return 0;
 }
 
@@ -120,7 +114,7 @@ void move_pointer(int dir) {
 	}
 }
 
-void calculate(int mode) {
+void calculate(vector<int> &num_array, int mode) {
 	switch (mode) {
 	case MINUS:
 		if (num_array[pointer] == 0)
