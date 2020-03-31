97a60883974716eb51a84d197dafeaa642492079
asomeJay <museum114@naver.com>
2020-03-31 16:16:57 +0900

    :sparkles: 현대자동차 뮤지컬 페이지 업데이트 언제되냐!

---
+++ Python/hyundai.py
@@ -0,0 +1,30 @@
+import threading
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import urllib3, requests
+import time
+
+url = "http://www.hyundaiufe.co.kr/"
+
+class AsyncTask:
+    def __init__(self):
+        pass
+    
+    def TaskA(self):
+        print(time.strftime('%Y-%m-%d-%p-%I-%M-%S', time.localtime(time.time())), end =" ")
+        source = requests.get(url).text
+        bs = BeautifulSoup(source, 'html.parser')
+        d = bs.find_all('h1')
+        if (d[4].string == "사이트 업데이트 중입니다") :
+            print("아직 사이트 업데이트 안됨")
+        else:
+            print("오오오오 사이트 바뀜!!!")
+        threading.Timer(10, self.TaskA).start()    
+    
+def main():
+    print ('START')
+    at=AsyncTask()
+    at.TaskA()
+
+if __name__ == "__main__":
+    main()
