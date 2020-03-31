bc548a5b047068e372c1fb4fa3f99c1858a2f3d3
asomeJay <museum114@naver.com>
2020-03-31 16:24:01 +0900

    :loud_sound: Add Alaram when Complete

--- Python/hyundai.py
+++ Python/hyundai.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import urllib3, requests
 import time
+import os
 
 url = "http://www.hyundaiufe.co.kr/"
 
@@ -18,6 +19,7 @@ class AsyncTask:
         if (d[4].string == "사이트 업데이트 중입니다") :
             print("아직 사이트 업데이트 안됨")
         else:
+            os.system('start C:\\Users\\KimJaeWon\\Downloads\\TickTok.mp3')
             print("오오오오 사이트 바뀜!!!")
         threading.Timer(10, self.TaskA).start()    
     
